8d174fd5cd39624cae79a3ecb99b80f195735e6b
Lucía Pancorbo <146762761+lpancorbo@users.noreply.github.com>
2024-05-14 21:42:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+  var member = document.getElementById('member');
+  var memberSkills = document.getElementById('skills');
+  member.style.display = 'none';
+  memberSkills.style.display = 'block';
+}
